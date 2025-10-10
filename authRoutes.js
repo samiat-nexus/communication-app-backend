06b229377251fc07f61823b9918bf97197fd0d43
samiat-nexus <contact.samiatnexus@gmail.com>
2025-10-11 02:24:54 +0600

    authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../user");
 
 const router = express.Router();
 
