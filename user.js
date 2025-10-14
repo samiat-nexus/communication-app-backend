0b73209e4c8f68b8dcb04b9800336bbe2a1fc74f
samiat-nexus <contact.samiatnexus@gmail.com>
2025-10-14 20:12:10 +0600

    user.js

--- user.js
+++ user.js
@@ -1,8 +1,9 @@
+// backend/models/user.js
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, default: "Anonymous" },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
@@ -10,8 +11,12 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
