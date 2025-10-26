5ed771d58a95fea5bf8e934712ce833afdafd764
samiat-nexus <contact.samiatnexus@gmail.com>
2025-10-26 11:51:30 +0600

    profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./models/user");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("./middleware/authMiddleware");
 
 // === GET PROFILE ===
 router.get("/", authMiddleware, async (req, res) => {
