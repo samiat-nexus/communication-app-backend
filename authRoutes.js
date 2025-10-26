d82c1538feda6375f3d05c6fdc0f1f65801d70b5
samiat-nexus <contact.samiatnexus@gmail.com>
2025-10-26 11:51:50 +0600

    authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("./models/user");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("./middleware/authMiddleware");
 
 // === SIGNUP ===
 router.post("/signup", async (req, res) => {
